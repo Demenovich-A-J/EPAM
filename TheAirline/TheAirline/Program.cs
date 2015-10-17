85071d95287382f60db2d67389474c201235aa64
Metiska <demenoff.aj@gmail.com>
2015-10-17 13:09:15 +0300

    Create class MyComparator to show how now work new sort.

--- TheAirline/TheAirline/Program.cs
+++ TheAirline/TheAirline/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,19 +10,32 @@ using TheAirline.Interfaces;
 
 namespace TheAirline
 {
+    class MyComparator : IComparer<IPlane>
+    {
+        public int Compare(IPlane x, IPlane y)
+        {
+            return -x.Range.CompareTo(y.Range);
+        }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
             Airline airline = new Airline("Newston", new List<IPlane>());
+            MyComparator myComparator = new MyComparator();
 
             airline.Add(new TransportPlane("Su", 5000, 300, 5000));
             airline.Add(new PassengerPlane("Boing",2500,150,40));
-            airline.Add(new PassengerPlane("Boing", 2500, 100, 80));
-            airline.Add(new PrivatePlane("Nowbel", 1000, 30, "Bill Geits"));
+            airline.Add(new PassengerPlane("Boing", 1500, 100, 80));
+            airline.Add(new PrivatePlane("Nowbel", 3000, 30, "Bill Geits"));
+
+
+            airline.Sort(myComparator);
 
             Console.WriteLine(airline);
-
+            Console.WriteLine(airline.TotalCapcity());
+            Console.WriteLine(airline.TotalLoadCapcity());
 
             Console.ReadKey();
 
